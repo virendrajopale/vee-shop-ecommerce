026bc152cb7ac91c0ae4f51ff41bd52e7051c5b2
Virendra Jopale <123791886+virendrajopale@users.noreply.github.com>
2024-06-21 17:12:12 +0530

    testing app.js
    testing

--- backend/app.js
+++ backend/app.js
@@ -46,8 +46,11 @@ app.use("/api/v1",orders)
 app.use("/api/v1",payment)
 
 app.use(express.static(path.join(__dirname,"../frontend/build")))
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+// })
+app.get('/',(req,res)=>{
+    res.json("Hello")
 })
 //middleware for error
 app.use(errormidle)
