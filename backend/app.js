3e1bf1a6d93e27528e154639e8e9caa375bbd310
Virendra Jopale <123791886+virendrajopale@users.noreply.github.com>
2024-06-21 17:16:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,23 +10,24 @@ if(process.env.NODE_ENV!=="PRODUCTION"){
 
     require('dotenv').config({path:"backend/config/config.env"})
 }
-
+const cors=require('cors')
+app.use(cors({
+    origin:['https://ecommerce-web-97ip.vercel.app/'],
+    methods:{"GET","POST","PUT","DELETE"],
+             credentials:true
+}))
 //route imports
 const products=require("./routes/productRoute")
 
 const user=require("./routes/userRoutes")
 
 const orders=require("./routes/orderRoute")
-const cors=require('cors')
+
 const payment=require("./routes/paymentRoute")
 const bodyParser=require('body-parser')
 const multer = require('multer')
 app.use(express.json())
-app.use(cors({
-    origin:['https://ecommerce-web-97ip.vercel.app/'],
-    methods:{"GET","POST","PUT","DELETE"],
-             credentials:true
-}))
+
 // app.use(express.json({ limit: "50mb" })); 
 app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as necessary
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Adjust the limit as necessary
@@ -37,15 +38,15 @@ app.use(cookieParser())
 app.use(fileUpload())
 
 
-app.use("/api/v1",products)
+// app.use("/api/v1",products)
 
-app.use("/api/v1",user)
+// app.use("/api/v1",user)
 
-app.use("/api/v1",orders)
+// app.use("/api/v1",orders)
 
-app.use("/api/v1",payment)
+// app.use("/api/v1",payment)
 
-app.use(express.static(path.join(__dirname,"../frontend/build")))
+// app.use(express.static(path.join(__dirname,"../frontend/build")))
 // app.get("*",(req,res)=>{
 //     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
 // })
